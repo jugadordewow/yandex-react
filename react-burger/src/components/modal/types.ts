d5780de3d759455de464b5cc6e4802d122b7a175
jugadordewow <sodfest@mail.ru>
2022-12-25 21:13:39 +0300

    Updates

--- react-burger/src/components/modal/types.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export interface IModal {
-        children?: React.ReactNode | undefined;
-        onClose?: () => void;
-}
--- react-burger/src/utils/ws-constants.ts
+++
@@ -1,4 +0,0 @@
-export const WS_URL_COMMON = 'wss://norma.nomoreparties.space'
-export const WS_URL_ALL = `${WS_URL_COMMON}/orders/all`;
-export const WS_URL_OWNER = `${WS_URL_COMMON}/orders`;
-
